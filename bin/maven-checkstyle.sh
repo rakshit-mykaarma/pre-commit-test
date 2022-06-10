0400c290f1e149e52c53cc34356c8bb512a32b06
Rakshit Kumar <rakshit.kumar@mykaarma.com>
2022-06-10 12:00:46 +0530

    Init

---
+++ bin/maven-checkstyle.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Running checkstyle linter on the newly added files(if any):"
+# retrieving current working directory
+CWD=`pwd`
+MAIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# go to main project dir
+cd $MAIN_DIR/../../
+# running maven checkstyle
+mvn checkstyle:check -Dincludes='**\\'$(git diff --cached --name-only --diff-filter=A | xargs -n1 basename | tr '\n' ' '| rev | cut -c 2- | rev | sed 's/ /,**\\/g')
+if [ $? -ne 0 ]; then
+  "Error while testing the code"
+  # go back to current working dir
+  cd $CWD
+  exit 1
+fi
+# go back to current working dir
+cd $CWD
