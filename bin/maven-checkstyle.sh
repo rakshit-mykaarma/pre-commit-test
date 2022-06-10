b53bfcfe95bd677f0d15cd685c33678995913e76
Rakshit Kumar <rakshit.kumar@mykaarma.com>
2022-06-10 12:20:09 +0530

    Test

--- bin/maven-checkstyle.sh
+++ bin/maven-checkstyle.sh
@@ -6,6 +6,8 @@ CWD=`pwd`
 MAIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # go to main project dir
 cd $MAIN_DIR/../../
+
+echo "\n\n\n\n"$(pwd)
 # running maven checkstyle
 mvn checkstyle:check -Dincludes='**\\'$(git diff --cached --name-only --diff-filter=A | xargs -n1 basename | tr '\n' ' '| rev | cut -c 2- | rev | sed 's/ /,**\\/g')
 if [ $? -ne 0 ]; then
